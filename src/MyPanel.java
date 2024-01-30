f1a129a6938f4140217810d86aaa5984b7806288
Geniy <Geniy@ya.com>
2024-01-30 17:34:44 +0300

    Вывод цыфр

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -15,36 +15,54 @@ public class MyPanel extends JPanel {
     private int y = 100;
     private int width = 200;
     private int height = 200;
+    private int step = 30;
     private int delta = 10;
 
     private ArrayList<Rectangle> outlist = new ArrayList<Rectangle>();
 
     public void SetSymbols(String param) {
-        segment(1);
-        segment(2);
-        segment(3);
-        segment(4);
-        segment(5);
-        segment(6);
-        segment(7);
+        String[] arr = param.replace(",", "").split(" ");
+        for (String cifra : arr) {
+            switch (cifra) {
+                case "1":
+                    segment(2);
+                    segment(4);
+                    break;
+                case "2":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(6);
+                    segment(5);
+                    break;
+                case "3":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(4);
+                    segment(5);
+                    break;
+            }
+            x = x + step + width;
+        }
     }
 
-    private void segment(int number) {
-        switch (number) {
+    private void segment(int param) {
+        switch (param) {
             case 1:
-                outlist.add(new Rectangle(x + delta, y, width - delta, y));
+                outlist.add(new Rectangle(x + delta, y, x + width - delta, y));
                 break;
             case 2:
-                outlist.add(new Rectangle(width, y + delta, width, y + (height / 2) - delta));
+                outlist.add(new Rectangle(x + width, y + delta, x + width, y + (height / 2) - delta));
                 break;
             case 3:
-                outlist.add(new Rectangle(x + delta, y + height / 2, width - delta, y + height / 2));
+                outlist.add(new Rectangle(x + delta, y + height / 2, x + width - delta, y + height / 2));
                 break;
             case 4:
-                outlist.add(new Rectangle(width, y + (height / 2) + delta, width, y + height - delta));
+                outlist.add(new Rectangle(x + width, y + (height / 2) + delta, width + x, y + height - delta));
                 break;
             case 5:
-                outlist.add(new Rectangle(x + delta, y + height, width - delta, y + height));
+                outlist.add(new Rectangle(x + delta, y + height, x + width - delta, y + height));
                 break;
             case 6:
                 outlist.add(new Rectangle(x, y + (height / 2) + delta, x, y + height - delta));
